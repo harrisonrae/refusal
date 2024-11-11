cfe5db5a1cea16c9e86981f40540019d2cbd2fac
haryear <harry-rae@hotmail.com>
2024-11-11 20:39:38 +1100

    commit changes

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript function to toggle the display of the text box
-function toggleTextBox(imageElement) {
-    const textBox = imageElement.nextElementSibling;
-    if (textBox.style.display === "none" || textBox.style.display === "") {
-        textBox.style.display = "flex";
-    } else {
-        textBox.style.display = "none";
-    }
-}
